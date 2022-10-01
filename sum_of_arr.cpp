ee8516b5bb297cfca1028f3a96a8059be78c88df
Ravi Kumar <99571109+Ravikumar2002@users.noreply.github.com>
2022-10-01 10:09:38 +0530

    Add files via upload

---
+++ sum_of_arr.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int getsum(int arr[],int n){
+	int sum =0;
+	if(n%2!=0){
+		return 0;
+	}
+	for(int i=0;i<n;i++){
+		sum=sum+arr[i];
+	}
+	return sum;
+}
+int main(){
+	int n;
+	cout<<"Enter the number"<<endl;
+	cin>>n;
+
+	int arr[n];
+
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+
+	cout<<getsum(arr,n);
+
+
+	return 0;
+
+}
